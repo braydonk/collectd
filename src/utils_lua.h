e1c44b743bca01a13513e476f2b1114445d54da2
yinzhi7 <sophieyfang@google.com>
2019-10-15 15:10:57 -0400

    Stop poisoning function in debug mode. (#153)

--- src/utils_lua.h
+++ src/utils_lua.h
@@ -30,9 +30,6 @@
 #include "plugin.h"
 #include "collectd.h"
 
-#ifndef DONT_POISON_SPRINTF_YET
-#error "Files including utils_lua.h need to define DONT_POISON_SPRINTF_YET."
-#endif
 #include <lua.h>
 
 /*
